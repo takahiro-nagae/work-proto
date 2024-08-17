90fe8cc79f65116bb13a07eb20acc6e184111d1b
takahiro-nagae <takahiro-nagae@cybozu.co.jp>
2024-08-17 19:52:34 +0900

    __init__.pyを各プロダクションコードのディレクトリ追加

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 from flask import Flask, render_template, request
 
-from analyze_number import routes as analyze_number
-from items import routes as items
-from template_filter import resource_filter
+from analyze_number.routes import analyze_number_bp
+from items.routes import items_bp
+from template_filter.resource_filter import bool_to_symbol
 
 
 def create_app():
     app = Flask(__name__)
-    app.register_blueprint(analyze_number.analyze_number_bp)
-    app.register_blueprint(items.items_bp)
+    app.register_blueprint(analyze_number_bp)
+    app.register_blueprint(items_bp)
 
-    app.template_filter('bool_to_symbol')(resource_filter.bool_to_symbol)
+    app.template_filter('bool_to_symbol')(bool_to_symbol)
 
     @app.route('/')
     def home():
