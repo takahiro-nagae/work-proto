fb45785582a3342e0b1f80b5ccd437844984d550
takahiro-nagae <takahiro-nagae@cybozu.co.jp>
2024-08-17 18:22:55 +0900

    素数判定追加

--- analyze_number/analyze_number.py
+++ analyze_number/analyze_number.py
@@ -7,13 +7,31 @@ def is_even(num):
     return num % 2 == 0
 
 
+def is_prime(num):
+    if num <= 1:
+        return False
+
+    if num == 2:
+        return True
+
+    if num % 2 == 0:
+        return False
+
+    for i in range(3, num + 1, 2):
+        if num % i == 0:
+            return i == num
+
+    return True
+
+
 @app.route('/analyze_number', methods=['POST'])
 def analyze_number():
     num = int(request.form.get('num'))
 
     data = {
         'number': num,
-        'is_even':  is_even(num)
+        'is_even': is_even(num),
+        'is_prime': is_prime(num)
     }
 
     return render_template('analyze_number.html', **data)
--- tests/analyze_number/test_logic.py
+++ tests/analyze_number/test_logic.py
@@ -1,6 +1,6 @@
 import pytest
 
-from analyze_number.analyze_number import is_even
+from analyze_number.analyze_number import is_even, is_prime
 
 
 def test_is_even():
@@ -9,6 +9,21 @@ def test_is_even():
     assert is_even(0) is True
     assert is_even(-2) is True
     assert is_even(-3) is False
+
+
+def test_is_prime():
+    assert is_prime(-1) is False
+    assert is_prime(0) is False
+    assert is_prime(1) is False
+    assert is_prime(2) is True
+    assert is_prime(3) is True
+    assert is_prime(4) is False
+    assert is_prime(5) is True
+    assert is_prime(6) is False
+    assert is_prime(7) is True
+    assert is_prime(47) is True
+    assert is_prime(91) is False
+    assert is_prime(1399) is True
 
 
 if __name__ == '__main__':
